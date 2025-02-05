64556bb11cecc97d2dba8d748f1d6aeb31c2be36
Elasto-long <sales.elastomech@gmail.com>
2025-02-05 10:53:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App.jsx";
-import "../src/app.css";
+// import "../src/app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/elastomech">
